cb19f64fbf6491e1f1ee75263be90f7beea5cf46
InfluenzaDIP <suweiheng@jlu.edu.cn>
2024-11-25 17:01:03 +0800

    Delete Influenza-virus-DI-identification-upstream directory

--- Influenza-virus-DI-identification-upstream/2.4.QCmap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for each in `ls *.sam`
-do 
-	samtools view -hS -F 4 -q 20 $each | grep -E "@|AS:i" | grep -v "XS:" | samtools view -bS | samtools sort -O bam -o BAM/${each%.sam}_HQ.bam
-	samtools depth -a BAM/${each%.sam}_HQ.bam > depth/${each%.sam}_HQ.depth
-done
